9cb6bced43fab2997115de19a981bd47283f1e05
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-08-03 16:29:49 +0200

    create user model in db

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,15 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
+import sequelize from "./config/database";
+import User from "./models/user";
+import authRoutes from "./routes/auth";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Welcome to TrailGuardians API");
+sequelize.sync({ force: false }).then(() => {
+  console.log("Database synced");
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`The server is running on port ${PORT}`);
-});
+export default app;
---
+++ src/routes/auth.ts
@@ -0,0 +1,32 @@
+import { Router, Request, Response } from "express";
+import User from "../models/user";
+
+const router = Router();
+
+router.post("/signup", async (res: Response, req: Request) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const user = await User.create({ name, email, password });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
+
+router.post("/login", async (res: Response, req: Request) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (user && user.password === password) {
+      res.status(201).json(user);
+    } else {
+      res.status(500).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(401).json({ error: (error as Error).message });
+  }
+});
+
+export default router;
