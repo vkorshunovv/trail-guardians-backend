ca913d99cc782ddc32544741dc6c20d87102e8f7
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-08-03 20:42:12 +0200

    add bcrypt library for hashing password

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import User from "../models/user";
+import bcrypt from "bcrypt";
 
 const router = Router();
 
@@ -7,7 +8,8 @@ router.post("/signup", async (res: Response, req: Request) => {
   const { name, email, password } = req.body;
 
   try {
-    const user = await User.create({ name, email, password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
     res.status(201).json(user);
   } catch (error) {
     res.status(500).json({ error: (error as Error).message });
